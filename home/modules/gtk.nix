8df2c4ed004a3a241e355bea02e06188a4cdd0d8
Neel Narayan Shetty <neelnarayanshetty@gmail.com>
2024-08-04 18:16:16 +0530

    Updates

--- home/modules/gtk.nix
+++ home/modules/gtk.nix
@@ -15,11 +15,11 @@
     gtk3.extraConfig = { gtk-application-prefer-dark-theme = 1; };
     gtk4.extraConfig = { gtk-application-prefer-dark-theme = 1; };
   };
-  
+
   qt = {
     enable = true;
-    style.name = "adwaita-dark";
-    platformTheme.name = "gtk3";
+    platformTheme.name = "kvantum";
+    style.name = "kvantum";
   };
 }
 
