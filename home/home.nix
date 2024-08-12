6240488549966ebaaca1b506f51ab5323960bfff
Neel Narayan Shetty <neelnarayanshetty@gmail.com>
2024-08-13 02:34:44 +0530

    Updates

--- home/home.nix
+++ home/home.nix
@@ -3,7 +3,7 @@
 {
   home.username = "neel";
   home.homeDirectory = "/home/neel";
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.stateVersion = "24.05";
 
   imports = [
     ./modules/zsh/zsh.nix
@@ -19,16 +19,11 @@
     ./modules/wlogout/wlogout.nix
   ];
 
-  # inputs.blender-bin.packages.x86_64-linux.blender_4_1.enable = true;
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     signal-desktop
     distrobox
     tmux
     gamescope
-    jdk22
     ryujinx
     protonvpn-gui
     pureref
@@ -124,35 +119,18 @@
 
     # programming languages
     python3
+    jdk22
     nodejs_22
     go
     rustup
     zig
     gcc
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
+    # fonts
     (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
+    # scripts
     (import ../scripts/gacp.nix { inherit pkgs; })
-    # (pkgs.writeShellScriptBin "gacp" ''
-    #   if [ -z "$1" ]; then
-    #     ${pkgs.figlet}/bin/figlet "No Commit Message"
-    #     echo "Using \"Updates\" as commit message instead"
-    #     git add .
-    #     git commit -m "Updates"
-    #     git push
-    #   else
-    #     git add .
-    #     git commit -m "$1"
-    #     git push
-    #   fi
-    # '')
   ];
 
   programs.nix-index-database.comma.enable = true;
@@ -166,46 +144,17 @@
     size = 16;
   };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     ".config/btop/btop.conf".source = ./modules/btop/btop.conf;
     ".config/btop/themes/catppuccin-mocha.theme".source =
       ./modules/btop/catppuccin-mocha.theme;
     ".config/obs-studio/themes".source = ./modules/obs/themes;
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/neel/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "nvim";
     NIXOS_OZONE_WL = "1";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
